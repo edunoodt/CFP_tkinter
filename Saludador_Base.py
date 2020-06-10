6eadcd28facba6f5641f333c3d773fca7fbb3482
edunoodt <edunoodt@gmail.com>
2020-06-10 08:47:17 -0300

    Ejercicios del CFP34 sobre uso de GUI's

--- Saludador_Base.py
+++ Saludador_Base.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
+
+
 def btnSaludar():
     texto='¡¡¡Hola '+txtNombre.get()
     if flagSonrisa.get() == 1:
@@ -11,16 +13,18 @@ def btnSaludar():
 
 root = tk.Tk()
 root.title('Saludador')
+root.geometry('400x150')
 
-btnSaludar = tk.Button(root,text='Saludar',command=btnSaludar)
+
+btnSaludar = tk.Button(root,text='Saludar',fg='white',bg='blue',command=btnSaludar)
 btnSaludar.grid(row=2,column=0)
-lblTexto = tk.Label(root,text='¿A quien saludamos?')
+lblTexto = tk.Label(root,text='¿A quien saludamos?',font=('Arial',30))
 lblTexto.grid(row=0,column=0,columnspan=2)
 txtNombre = tk.Entry(root)
 txtNombre.grid(row=1,column=0)
 flagSonrisa=tk.IntVar()
 chkSonrisa = tk.Checkbutton(root,text = 'Agregar :)', variable=flagSonrisa)
-chkSonrisa.grid(row=3,column=0)
+chkSonrisa.grid(row=1,column=1)
 
 root.mainloop()
 
