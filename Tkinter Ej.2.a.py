7044102d63f5ad98ab6bb91ac565d129286632b8
edunoodt <edunoodt@gmail.com>
2020-06-15 10:05:43 -0300

    Version final de la aplicación para calcular los intereses.  Se corrigió el cálculo para 2 o menos cuotas.  Se agregaron los botones de borrado y salida de la ventana
    Signed-off-by: edunoodt <edunoodt@gmail.com>

--- Tkinter Ej.2.a.py
+++ Tkinter Ej.2.a.py
@@ -1,54 +1,97 @@
 from tkinter import *
 
+
+# Define la ventana principal
+main = Tk()
+main.title('Calculadora de Financiación')
+
+C = StringVar()
+F = StringVar()
+I = StringVar()
+
+# Función convocada desde el boton de cálculo de la financiacón.
+# Utiliza la formula de calculo de valor actual de una renta fija prepaga:
+# Va=R * {[1-(1+tasa)^(-n)]/tasa} * (1+tasa) donde:
+# tasa: es el valor de la tasa anual por año, de interés compuesto
+# y es el informado por el enunciado
+# n: Es el número de períodos seleccionados que no puede exceder de 18
+# R: es la renta o cuota, que despejada de esta formula y
+# nos permite encontrar el valor buscado)
 def valor_cuota ():
     total = float(monto_Entry.get())
-    cuotas = float(cuotas_Entry.get())
-    int_dic={'0-2':0,'3-6':0.1,'7-12':0.2,'13-18':0.3}
-    if cuotas < 19:
-        if cuotas < 3:
-            int=int_dic['0-2']
-        elif 3<=cuotas<7:
-            int=int_dic['3-6']
-        elif cuotas<13:
-            int=int_dic['7-12']
+    num_cuotas = float(cuotas_Entry.get())
+    int_dic = {'0-2': 0, '3-6': 0.1, '7-12': 0.2, '13-18': 0.3}
+    if num_cuotas < 19:
+        if num_cuotas > 2:
+            if 3 <= num_cuotas < 7:
+                int = int_dic['3-6']
+            elif num_cuotas < 13:
+                int = int_dic['7-12']
+            else:
+                int = int_dic['13-18']
+            cuota = round((total / (1 - (1 + int) ** (-num_cuotas))) * int / (1 + int), 2)
         else:
-            int=int_dic['13-18']
-
-        cuota = round((total / (1 - (1 + int) ** (-cuotas))) * int / (1 + int),2)
-        ValorCuota_Lbl.configure(text='$' + str(cuota), relief='sunken', bg='white', font=('arial', 12))
+            cuota = round(total / num_cuotas, 2)
+        pagoTotal = round(cuota * num_cuotas, 2)
+        intereses = round(pagoTotal-total, 2)
+        C.set('$'+str(cuota))
+        F.set('$'+str(pagoTotal))
+        I.set('$'+str(intereses))
     else:
-        ValorCuota_Lbl.configure(text='18 cuotas màximo', relief='sunken', bg='white', font=('arial', 12))
+        C.set('18 cuotas màximo')
 
-main = Tk()
+# Funcion ejecutada por el boton borrar
+def borrar():
+    monto_Entry.delete(first=0,last=22)
+    monto_Entry.focus()
+    cuotas_Entry.delete(first=0,last=22)
+    ValorCuota_Entry.delete(first=0,last=22)
+    ValorFinal_Entry.delete(first=0,last=22)
+    ValorIntereses_Entry.delete(first=0,last=22)
 
-main.title('Calculadora de Financiación')
-#main.geometry ('400x200')
+# Funcion para salir del loop y cerrar la ventana
+def salir():
+    main.destroy()
 
-monto_Lbl=Label(text='Pago Total')
-monto_Lbl.grid(row=0,column=0)
+
+#Ingreso del monto total del producto o total a financiar (texto y ventana de entrada)
+monto_Lbl=Label(text='Pago Total').grid(row=0,column=0)
 monto_Entry = Entry(main)
 monto_Entry.grid(row=0,column=1, padx=20, pady=20)
 
-cuotas_Lbl=Label(text='Cantidad de cuotas')
-cuotas_Lbl.grid(row=1,column=0)
+# Ingreso de la acntidad de cuotas (texto y ventana de entrada)
+cuotas_Lbl=Label(text='Cantidad de cuotas').grid(row=1,column=0)
 cuotas_Entry = Entry(main)
-cuotas_Entry.grid(row=1,column=1, padx=20, pady=20)
+cuotas_Entry.grid(row=1, column=1, padx=20, pady=20)
 
+# Presentación del valor de la cuota
+ValorCuotaTxt_Lbl = Label(main, text='Valor de la cuota:').grid(row=2 , column=0)
+ValorCuota_Entry = Entry(main, textvariable=C)
+ValorCuota_Entry.grid(row=2, column=1, padx=20, pady=20)
 
-ValorCuotaTxt_Lbl= Label(main,text='Valor de la cuota:')
-ValorCuotaTxt_Lbl.grid(row=2 , column=0)
-ValorCuota_Lbl = Label(main,text='Ingrese Monto total y cantidad de cuotas',relief='sunken',bg='white',font=('arial',8))
-ValorCuota_Lbl.grid(row=2 , column=1, padx=20, pady=20)
+# Pago total o suma de todas las cuotas. No tiene significación financiera, ya que son
+# pagos realizados en distintos  momentos.
+# Para poder sumarlos habría que llevarlos al mismo momento.
+ValorFinalTxt_Lbl= Label(main,text='Valor final pagado:')
+ValorFinalTxt_Lbl.grid(row=3, column=0)
+ValorFinal_Entry = Entry(main, textvariable=F)
+ValorFinal_Entry.grid(row=3, column=1, padx=20, pady=20)
 
-"""
-valorCuota_Lbl=Label(text='Valor de cada cuota')
-valorCuota_Lbl.grid(row=2,column=0)
-valorCuota_Entry = Entry(main)
-valorCuota_Entry.grid(row=2,column=1)"""
+# Intereses Pagados.  Diferencia entre el monto por unica vez y la suma de todos los pagos
+ValorInteresesTxt_Lbl= Label(main, text='Intereses:')
+ValorInteresesTxt_Lbl.grid(row=4, column=0)
+ValorIntereses_Entry = Entry(main, textvariable=I)
+ValorIntereses_Entry.grid(row=4, column=1, padx=20, pady=20)
 
-monto_BTN = Button(text = 'calcule la Financiación', command = valor_cuota, width=50)
-monto_BTN.grid(row=3, columnspan=2, pady=20)
+# Botón para ejecutar los cálculos de la función "valor_cuota"
+monto_BTN = Button(main, text = 'calcule la Financiación', command = valor_cuota, width=50)
+monto_BTN.grid(row=5, columnspan=2, pady=20)
 
+# Boton para limpiar y reiniciar el cálculo
+nuevo_BTN = Button(main, text='Borrar', command=borrar).grid(row=6, column=0)
+
+# Boton para salir del loop y cerrar la ventana
+Salir_BTN = Button(main, text='Cerrar la ventana', command=salir).grid(row=6, column=1)
 
 
 mainloop()
